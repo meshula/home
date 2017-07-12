78e8cd3cc5e99327e96820ab6921f8eee293e1f7
Nick Porcino <meshula@hotmail.com>
2017-07-12 16:23:36 -0700

    fix lola command

--- git-config.sh
+++ git-config.sh
@@ -1,4 +1,4 @@
 #/bin/sh
-git config --global alias.lol 'log --oneline --graph --decorate'
-git config --global alias.lola '--oneline --all --branches --graph --decorate'
+git config --global alias.lol "log --oneline --graph --decorate"
+git config --global alias.lola "log --oneline --all --branches --graph --decorate"
 
